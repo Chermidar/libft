15689981685bd30d81b307e7cfa70bce8f4886e0
Carlos Hermida ron <chermida@c1r10s6.42madrid.com>
2022-07-14 17:20:30 +0200

    Terminado itoa

---
+++ ft_strmapi.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chermida <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 15:42:51 by chermida          #+#    #+#             */
+/*   Updated: 2022/07/14 16:35:57 by chermida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	
+}
+/*
+A cada carácter de la string ’s’, aplica la
+de ’f’
+función ’f’ dando como parámetros el índice de cada
+carácter dentro de ’s’ y el propio carácter. Genera
+una nueva string con el resultado del uso sucesivo
+*/
